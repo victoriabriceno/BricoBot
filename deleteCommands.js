ef0be46ce6e95da49794a95aa18cc9713b0fd8f1
Victoria Briceno <97036784+victoriabriceno@users.noreply.github.com>
2022-11-12 16:47:45 -0500

    delete command

--- deleteCommands.js
+++ deleteCommands.js
@@ -7,3 +7,4 @@ const client = new discord.Client();
 client.api.applications(client.user.id).commands("1032692482917531790").delete(); // random already delete but it was still showing
 client.api.applications(client.user.id).commands("1035337796166107246").delete(); // specif-artist duplicate
 client.api.applications(client.user.id).commands("1035337795104948344").delete();// suggest duplicate
+client.api.applications(client.user.id).commands("1039996149219741777").delete();// play-song
